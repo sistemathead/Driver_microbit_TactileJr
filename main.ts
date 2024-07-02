9afbefe09c3d7e88fa3b64f49a98496c0e534a0c
Sistema THEAD, non-profit education <105808588+sistemathead@users.noreply.github.com>
2024-07-02 14:46:23 +0200

    Actualització pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,5 +7,27 @@ bluetooth.onBluetoothDisconnected(function () {
 })
 bluetooth.onUartDataReceived(serial.delimiters(Delimiters.Comma), function () {
     basic.showString(bluetooth.uartReadUntil(serial.delimiters(Delimiters.Comma)))
+    if (bluetooth.uartReadUntil(serial.delimiters(Delimiters.Comma)) == "adelente") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 255)
+        basic.pause(500)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+    if (bluetooth.uartReadUntil(serial.delimiters(Delimiters.Comma)) == "atras") {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 255)
+        basic.pause(500)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+    if (bluetooth.uartReadUntil(serial.delimiters(Delimiters.Comma)) == "izquierda") {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 255)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 255)
+        basic.pause(500)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+    if (bluetooth.uartReadUntil(serial.delimiters(Delimiters.Comma)) == "derecha") {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 255)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 255)
+        basic.pause(500)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
 bluetooth.startUartService()
